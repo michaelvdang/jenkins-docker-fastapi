034fccb6166b04660e3fb42a4e23bffa14c1e830
Michael Dang <dominhation@hotmail.com>
2024-02-08 15:58:14 -0800

    testing fastapi

--- test-base/test.sh
+++ test-base/test.sh
@@ -2,8 +2,8 @@ curl fa-cont
 sleep 1
 curl fa-cont/static 
 sleep 1
-curl fa-cont/dynamic/Roman
-sleep 1
-echo '' > output.txt
-echo 'output.txt content: '
-cat output.txt
+curl fa-cont/dynamic/Roman > output.txt
+# sleep 1
+# echo '' > output.txt
+# echo 'output.txt content: '
+# cat output.txt
