06af3cbf1f692eb88b040d17947a19b679d58372
Michael Dang <dominhation@hotmail.com>
2024-02-09 14:00:42 -0800

    add sleep to test.sh

--- test-base/test.sh
+++ test-base/test.sh
@@ -1,10 +1,16 @@
 mkdir -p data
 curl fa-cont:4000 > /data/output2.txt
+sleep 3
 curl fa-cont:4000/static 
+sleep 3
 curl fa-cont:4000/dynamic/Roman >> /data/output2.txt
+sleep 3
 curl wv-cont:9200 >> /data/output2.txt
+sleep 3
 curl wv-cont:9200/fake-endpoint >> /data/output2.txt
+sleep 3
 curl wv-cont:9200/word/house >> /data/output2.txt
+sleep 3
 # sleep 1
 # echo '' > output.txt
 # echo 'output.txt content: '
